10f52da2b7215e2b7f3a1b6ba6e5783e7d6ecfe8
andrescuel <camilo.andcu@gmail.com>
2025-01-11 09:10:54 -0500

    chore: prettier & eslint in frontend project

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,9 +3,11 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import eslintPluginJsxA11y from 'eslint-plugin-jsx-a11y'
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'vite.config.js'] },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
@@ -22,17 +24,15 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'jsx-a11y': eslintPluginJsxA11y,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      ...eslintPluginJsxA11y.configs.recommended.rules,
     },
   },
+  eslintConfigPrettier
 ]
