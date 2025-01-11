34fb21937c7d184e26f32b94f686a2a0bbbfe0fa
andrescuel <camilo.andcu@gmail.com>
2025-01-11 14:31:37 -0500

    feat: created LibraryCafe component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { LibraryCafe } from './components/LibraryCafe'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <LibraryCafe
+      name='The Quiet Quill'
+      location='123 Literary Lane, Biblioville'
+      owner='Amelia Penwright'
+      description='A cozy cafe for book lovers with an extensive collection of classics and a menu of artisan teas and pastries.'
+    />
   )
 }
 
-export default App
